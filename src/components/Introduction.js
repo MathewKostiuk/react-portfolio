3ef50b1c4f577656621b5e328d93e437689e5b0d
Mathew Kostiuk <mathewkostiuk@gmail.com>
2019-12-28 16:31:19 +0900

    Modified the colour palette of the site
    Also made some changes to the Introduction component to better suit
    the new colour scheme

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,16 +4,14 @@ import './Introduction.scss';
 
 export default function Introduction() {
 
-  useEffect(() => {
-    const introductionElement = document.querySelector('.introduction');
-    introductionElement.style.backgroundImage = `url(/images/coloured-paper.jpg)`;
-  });
-
   return(
     <section className='introduction'>
       <div className='introduction__text'>
         <h2 className='introduction__text__name'>Hi, my name's Mathew</h2>
         <p className='introduction__text__description'>I'm a full stack web developer.</p>
+        <a href='https://github.com/MathewKostiuk' target='_blank'>
+          <img className='introduction__icon' src='/images/mark-github.png'/>
+        </a>
       </div>
     </section>
   )
