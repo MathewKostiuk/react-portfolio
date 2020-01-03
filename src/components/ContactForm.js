09fdb40d0655bab46a9180b31ff2b99f1ddb9b8b
Mathew Kostiuk <mathewkostiuk@gmail.com>
2020-01-03 16:10:16 +0900

    added padding around the sections as well as changed labels to placeholder text

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,15 +6,13 @@ export default function ContactForm() {
 
   return (
     <section id='contact' className='contact-form'>
-      <h2 className='contact-form__heading'>Have an idea for a project? <span className='brand-text'>Let's work together </span>on it.</h2>
+      <h2 className='contact-form__heading'>Have an idea for a project? <span className='brand-text'>Let's work together.</span></h2>
       <form className='contact-form__form'>
         <div className="contact-form__input-wrapper">
-          <label htmlFor='name' className='contact-form__label'>Name</label>
-          <input type='text' id='name' className='contact-form__input' required></input>
+          <input type='text' id='name' className='contact-form__input' placeholder='Name' required></input>
         </div>
         <div className="contact-form__input-wrapper">
-          <label htmlFor='email' className='contact-form__label'>E-mail</label>
-          <input type='text' id='email' className='contact-form__input' required></input>
+          <input type='text' id='email' className='contact-form__input' placeholder='E-mail' required></input>
         </div>
         <div className="contact-form__input-wrapper">
           <textarea type='text' id='message' className='contact-form__input' placeholder='Write your message here' required></textarea>
