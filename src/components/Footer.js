022032dbec1e90017f660e928baeb044732142fa
Mathew Kostiuk <mathewkostiuk@gmail.com>
2020-01-11 19:25:54 +0900

    added in footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,17 @@ export default function Footer() {
 
   return(
     <footer className='footer'>
-
+      <ul>
+        <li>
+          <a>Github</a>
+        </li>
+        <li>
+          <a>LinkedIn</a>
+        </li>
+        <li>
+          <a>E-mail</a>
+        </li>
+      </ul>
     </footer>
   )
 }
