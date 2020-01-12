f3e5ecb699e8a426fd3e2819149dd334e444a3d7
Mathew Kostiuk <mathewkostiuk@gmail.com>
2020-01-12 14:10:01 +0900

    further styling of the project card component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,16 +5,16 @@ import './ProjectCard.scss';
 export default function ProjectCard() {
 
   return (
-    <article className="project-card">
+    <article className='project-card'>
       <div>
         <h2>Pixel Union</h2>
         <h3>Tier 2 Technical Support</h3>
       </div>
-      <div>
+      <div className='project-card__details'>
         <h4>Summary:</h4>
         <p>Find & report bugs, fix bugs, commit bugs, repeat.</p>
       </div>
-      <div>
+      <div className='project-card__details'>
         <h4>Technologies:</h4>
         <p>Shopify Liquid, HTML, CSS, & JavaScript</p>
       </div>
